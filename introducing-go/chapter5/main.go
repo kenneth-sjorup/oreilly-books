03adba0a9f2ec459ef1548f65daf6c31ce37a668
Kenneth Sjorup <cksjorup@gmail.com>
2019-10-14 21:48:05 -0400

    finished chapter 5

---
+++ introducing-go/chapter5/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	x := [5]float64{
+		98,
+		93,
+		77,
+		82,
+		83,
+	}
+
+	var total float64 = 0
+	for _, value := range x {
+		total += value
+	}
+	fmt.Println(total / float64(len(x)))
+
+	slice1 := []int{ 1, 2, 3 }
+	slice2 := append(slice1, 4, 5)
+	fmt.Println(slice1, slice2)
+
+	slice3 := make([]int, 2)
+	copy(slice3, slice1)
+	fmt.Println(slice1, slice3)
+
+	y := make(map[string]int)
+	y["key"] = 10
+	fmt.Println(y)
+
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name": "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name": "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name": "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name": "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name": "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name": "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name": "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name": "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name": "Flourine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name": "Neon",
+			"state": "gas",
+		},
+	}
+	if el, ok := elements["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
+
+	z := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+	sort.Ints(z)
+	fmt.Printf("The smallest number from the array is: %v\n", z[0])
+}
